5b7a1465fe937230bfce1a6211e46229f1f49bb5
Andy Loredo <ndy.loredo@gmail.com>
2023-08-15 11:32:34 -0400

    Finished header (with hardcoded cart item amount)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => {
-  return <button>
-    <span></span>
-    <span></span>
-    <span></span>
+  return <button className={classes.button}>
+    <span className={classes.icon}>
+      <CartIcon />
+    </span>
+    <span>Your Cart</span>
+    <span className={classes.badge}>
+      3
+    </span>
   </button>
 }
 
